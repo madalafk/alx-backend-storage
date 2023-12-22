2ec3216fe6c5b13c26c33fa4431ccee1f40d36a4
madalafk <fmadala@hotmail.com>
2023-12-22 12:46:50 -0800

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Python function that returns all students sorted by average score with:
+Prototype: def top_students(mongo_collection).
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Prints all students in a collection sorted by average score.
+    """
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
